e608de02cdd73ac03f7ce88ca4c3781e00434fd6
PortlyMoney4024 <104855108+PortlyMoney4024@users.noreply.github.com>
2024-09-23 13:25:08 -0600

    update prueborly

--- Assets/Prueborly/Assembly/Prueborly.cs
+++ Assets/Prueborly/Assembly/Prueborly.cs
@@ -4,9 +4,85 @@ using UnityEngine;
 
 public class Prueborly : MonoBehaviour
 {
-    public float rotateSpeed = 50f;
+    public GameObject laser1;
+    //public GameObject laser2;
+
+    public float rotateSpeed;
+    public float moveSpeed;
+
+    Rigidbody rb;
+    public Transform player;
+
+    public float attackDuration = 3f;
+    public float attackInterval = 6f; // Tiempo mínimo entre ataques
+    public float attackRange = 10f;
+
+    private float lastAttackTime;
+    private float currentAttackTime;
+
+    //[SerializeField] PorlyRagdoll pr;
+
+    [SerializeField] Animator animator;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        laser1.SetActive(false);
+        //laser2.SetActive(false);
+        //pr = GetComponent<PorlyRagdoll>();
+        lastAttackTime = -attackInterval; // Configura el último tiempo de ataque para permitir el primer ataque
+
+        animator = GetComponentInChildren<Animator>();
+    }
+
     void Update()
     {
-        transform.Rotate(Vector3.up * Time.deltaTime * rotateSpeed);
+        if (pr.canAttack)
+        {
+            if (Time.time > lastAttackTime + attackInterval)
+            {
+                if (Vector3.Distance(transform.position, player.position) < attackRange)
+                {
+                    // Comienza un nuevo ataque
+                    if (currentAttackTime <= attackDuration)
+                    {
+                        laser1.SetActive(true);
+                        //laser2.SetActive(true);
+                        rb.MoveRotation(rb.rotation * Quaternion.Euler(0f, rotateSpeed * Time.deltaTime, 0f));
+                        currentAttackTime += Time.deltaTime;
+
+                        animator.SetBool("Attack", true);
+                    }
+                    // Finaliza el ataque y se inicia el temporizador para el próximo ataque
+                    else
+                    {
+                        laser1.SetActive(false);
+                        //laser2.SetActive(false);
+                        lastAttackTime = Time.time;
+                        currentAttackTime = 0f;
+                        animator.SetBool("Attack", false);
+                        transform.LookAt(player);
+                    }
+                }
+                // El enemigo no está en rango de ataque
+                else
+                {
+                    laser1.SetActive(false);
+                    //laser2.SetActive(false);
+                    currentAttackTime = 0f; // Reinicia el temporizador de ataque
+
+                    Vector3 direction = (player.position - transform.position).normalized;
+                    rb.MovePosition(transform.position + direction * moveSpeed * Time.deltaTime);
+                    transform.LookAt(player);
+                    animator.SetBool("Attack", false);
+                }
+            }
+
+        }
+
+        if (pr.vida <= 0)
+        {
+            laser1.SetActive(false);
+        }
     }
 }
